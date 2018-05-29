f7309e55254b20983e8cfef4f2f3684a9bb7cdeb
Prasen Palvankar <prasen.palvankar@gmail.com>
2018-05-28 23:43:19 -0700

    Added missing license headers

--- src/pages/recipe-form/recipe-form.ts
+++ src/pages/recipe-form/recipe-form.ts
@@ -1,3 +1,35 @@
+/*
+ *  MIT License
+ *
+ * Copyright (c) 2018 Prasen Palvankar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ *
+ *  File Created: Monday, 28th May 2018 11:15:42 pm
+ *  Author: Prasen Palvankar
+ *
+ *  Last Modified: Monday, 28th May 2018 11:39:52 pm
+ *  Modified By: Prasen Palvankar
+ */
+
 import { Recipe, RecipeServiceProvider } from './../../providers/recipe-service/recipe-service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
@@ -65,6 +97,12 @@ export class RecipeFormPage {
     }
 
 
+    /**
+     * Translate messages to be displayed in alerts and toasts
+     *
+     * @private
+     * @memberof RecipeFormPage
+     */
     private translate() {
         for (let messageId in this.messages) {
             this.translateService.get(messageId).subscribe(res => {
@@ -73,6 +111,10 @@ export class RecipeFormPage {
         }
     }
 
+    /**
+     * Show toast
+     * @param message
+     */
     private showToast(message: string) {
         let toast = this.toastCtrl.create({
             message: message,
@@ -84,6 +126,11 @@ export class RecipeFormPage {
     }
 
 
+    /**
+     * Handle "Save" click
+     *
+     * @memberof RecipeFormPage
+     */
     public submit() {
         console.log(this.addRecipeForm);
         for (let key of Object.keys(this.addRecipeForm.controls)) {
@@ -94,8 +141,5 @@ export class RecipeFormPage {
         this.navCtrl.pop();
     }
 
-    ionViewDidLoad() {
-        // console.log('ionViewDidLoad RecipeFormPage');
-    }
 
 }
